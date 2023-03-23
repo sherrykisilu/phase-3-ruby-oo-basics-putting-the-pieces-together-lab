efcbddf831322507ee51839994b3f06f43c5c19e
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-03-23 16:23:03 +0300

    done]

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
 
-
+class Book
+    attr_accessor :title,:author,:page_count,:genre,:turn_page
+    #attr_reader :
+    #attr_writer :
+    def initialize(title)
+        @title=title
+    end
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+end
